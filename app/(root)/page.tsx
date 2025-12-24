69908a2fca1623daa08dd6a5ac0ea36dd86c6660
Arjunsing Pardeshi <pardeshiarjunsing1010@gmail.com>
2025-12-24 15:48:05 +0530

    feat: add homepage design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,33 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { onBoardUser } from "@/modules/auth/actions";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import { useEffect } from "react";
+import ProjectForm from '@/modules/home/components/ProjectForm'
+import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
-  // useEffect(() => {
-  //   const runOnboarding = async () => {
-  //       await onBoardUser();
-  //     };
-
-  //     runOnboarding();  
-  // }, []);
+const Page = () => {
+  
   return (
-    <div className="flex min-h-screen items-center justify-center ">
-      <Button className="">click me</Button>
-      <UserButton/>
+    <div className='flex items-center justify-center w-full px-4 py-8'>
+      <div className='max-w-5xl w-full'>
+        <section className='space-y-8 flex flex-col items-center'>
+          <div className='flex flex-col items-center'>
+            <Image src={"/logo.svg"}
+            width={100}
+            height={100}
+            alt='Logo'
+            className='hidden md:block invert dark:invert-0'
+            />
+          </div>
+          <h1 className='text-2xl md:text-5xl font-bold text-center'>Describe it. Generate it. Ship it.</h1>
+          <p className='text-lg md:text-xl text-muted-foreground text-center'>
+            Creates apps and website by chatting with AI
+          </p>
+          <div className='max-w-3xl w-full'>
+            {/* {ProkectForm} */}
+            <ProjectForm/>
+          </div>
+        </section>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Page
