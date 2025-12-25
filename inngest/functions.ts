6b079d38092b4c7aee3bc448f21b3a2a91a5ac56
Arjunsing Pardeshi <pardeshiarjunsing1010@gmail.com>
2025-12-25 16:37:13 +0530

    feat: setup E2E flow with Inngest sandbox and Docker template

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,12 +1,18 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import Sandbox from "@e2b/code-interpreter"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "agent/hello" },
   async ({ event, step }) => {
     
-    
+    const sandboxId = await step.run("get-sandbox-id", async() =>{
+      const sandbox = await Sandbox.create("v0-clone-nextjs-002")
+      return sandbox.sandboxId
+    })
+
+
     const helloAgent = createAgent({
     name:'hello-agent',
     description: 'simple agent to say hello',
@@ -14,6 +20,12 @@ export const helloWorld = inngest.createFunction(
     model: gemini({model: 'gemini-2.5-flash'})
     });
     const {output} = await helloAgent.run("greet to the user")
+
+    const sandboxUrl = await step.run("get-sandbox-url", async() =>{
+      const sandbox = await Sandbox.connect(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`
+    })
 
     return{
       message: output[0].content
